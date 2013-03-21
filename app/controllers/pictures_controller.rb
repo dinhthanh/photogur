64d7dd7cb970ba1b949d2d86e4316cb752941cb0
Tommy Kail <tommy.kail@icloud.com>
2013-03-21 17:18:50 -0400

    Text color change

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -14,41 +14,20 @@ class PicturesController < ApplicationController # NOT: ActionController::Base
   end
 
   def update
-    # get the picture I want to update
     @picture = Picture.find(params[:id])
-
-    # to get the new data for this picture use the params Hash
-
-    # get the new data for this picture
-    success = @picture.update_attributes(:artist => params[:artist],:title => params[:title], :url => params[:url]) 
-    # @user.update_attributes(:status => 'active')
-
-    # params[:artist]
-    # params[:title]
-    # params[:url]
-    # update the picture
-    # update_attributes
-    # redirect to the picture so I can see my changes
-
-    if success
-    redirect_to '/pictures'
-    end
+    if @picture.update_attributes(params[:picture])
+        redirect_to @picture
+      end
   end
 
   def create
-    #@picture = Picture.new
-    #@picture.url = params[:url]
-    #@picture.title = params[:title]
-    #@picture.artist = params[:artist]
-    #well_done = @picture.save
-    #if well_done
-     # redirect_to pictures_path #same as '/picture'
-    #end
-    #render :text => "Save pic. Url: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}" 
-    if @picture = Picture.create!(params[:picture])
-      redirect_to '/pictures'
-    end
-    #success = picture.save
+    @picture = Picture.new(params[:picture])
+      if @picture.save
+        redirect_to @picture
+      else
+        flash.now[:error] = "Could not save the picture"
+        render :new
+      end
   end
 
   def edit
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,4 +2,30 @@ class Picture < ActiveRecord::Base
 	# Allow mass-assingment for the attributes title and artist
 	attr_accessible :title, :artist, :url
 	# Don't confuse with attr_accessor
+  validates :title, :presence => true
+  validates :url, :presence => true
 end
+ 
+# >> p = Person.new
+# => #<Person id: nil, name: nil>
+# >> p.errors
+# => {}
+ 
+#>> p.valid?
+#=> false
+#>> p.errors
+#=> {:name=>["can't be blank"]}
+ 
+#>> p = Person.create
+#=> #<Person id: nil, name: nil>
+#>> p.errors
+#=> {:name=>["can't be blank"]}
+ 
+#>> p.save
+#=> false
+ 
+#>> p.save!
+#=> ActiveRecord::RecordInvalid: Validation failed: Name can't be blank
+ 
+#>> Person.create!
+#=> ActiveRecord::RecordInvalid: Validation failed: Name can't be blank
